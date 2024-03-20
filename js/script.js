235906d271fe85fecbd78cf91a1cad5dc5b6b4ad
Michael <lightbar200@gmail.com>
2024-03-20 01:11:30 -0500

    added placeholders and eventListener

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
- HEAD
+
 //test from desktop
 
 //test from laptop
@@ -6,4 +6,33 @@
 //second test from laptop
 
 //test from desktop
+
+const guessLetter = document.querySelector(".guessed-letters");
+const guessButton =document.querySelector(".guess");
+const letterInput =document.querySelector("letter");
+const challange =document.querySelector(".word-in-progress");
+const chances =document.querySelector(".remaining");
+const chancesSpan =document.querySelector("remaining.span");
+const hint =document.querySelector(".message");
+const redo =document.querySelector(".play-again");
+
+const word = "magnolia";
+
+const progress = function (word) {
+    const progressLetters = [];
+    for (const letter of word) {
+        console.log(letter);
+        progressLetters.push("●")
+    }
+    challange.innerText = progressLetters.join("");
+};
+
+progress(word);
+
+guessButton.addEventListener ("click" , function (e) {
+    e.preventDefault();
+    const guess = letterInput.value;
+    console.log(guess)
+    letterInput,value = "";
+});
 
